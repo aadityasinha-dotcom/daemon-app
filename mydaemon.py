37909ee0c679ec83625e2352193fe07243dff3e2
aadityasinha-dotcom <aadityasinha009@gmail.com>
2024-02-07 22:03:48 +0530

    changes

--- mydaemon.py
+++ mydaemon.py
@@ -41,10 +41,6 @@ class MyDaemon(DaemonContext):
     
     def stop(self):
         print("Stopping MyDaemon...")
-        # Terminate any running processes
-        for process in psutil.process_iter():
-            if process.name() == "your_process_name":
-                process.terminate()
         self.stop()
 
 # main class for the program
